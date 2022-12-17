27c7e3640bd4a20d20ae04b0b67cff732abeaa5e
PuSung <sam90.hsu@gmail.com>
2022-12-17 17:58:02 +0800

    init peices

--- backend/chess_logic/classes/Pieces.js
+++ backend/chess_logic/classes/Pieces.js
@@ -9,7 +9,41 @@ class Piece {
 class Pawn extends Piece {
     constructor( x, y, clr ) {
         super( x, y, clr )
-
         this.type = 'pawn'
+    }
+}
+
+class Bishop extends Piece {
+    constructor( x, y, clr ) {
+        super( x, y, clr )
+        this.type = 'bishop'
+    }
+}
+
+class Rook extends Piece {
+    constructor( x, y, clr ) {
+        super( x, y, clr )
+        this.type = 'rook'
+    }
+}
+
+class Knight extends Piece {
+    constructor( x, y, clr ) {
+        super( x, y, clr )
+        this.type = 'knight'
+    }
+}
+
+class King extends Piece {
+    constructor( x, y, clr ) {
+        super( x, y, clr )
+        this.type = 'king'
+    }
+}
+
+class Queen extends Piece {
+    constructor( x, y, clr ) {
+        super( x, y, clr )
+        this.type = 'queen'
     }
 }
