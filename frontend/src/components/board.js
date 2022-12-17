85b39f4346fdbed80851619311d44fa35af99fc4
yclee6262 <yclee6262@gmail.com>
2022-12-17 19:02:11 +0800

    create board

---
+++ frontend/src/components/board.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Square from './square';
+
+function isEven(num) {
+    return num % 2 === 0
+}
+
+function Board() {
+    const board = [];
+    for (let i = 0; i < 8; i++) {
+      const squareRows = [];
+      for (let j = 0; j < 8; j++) {
+        const squareShade = (isEven(i) && isEven(j)) || (!isEven(i) && !isEven(j)) ? "white" : "black";
+        squareRows.push(<Square key={i} color={squareShade} position={i} />);
+      }
+      console.log(squareRows)
+      board.push(<div className="board-row" key={i} style={{display: "flex"} }>{squareRows}</div>)
+      
+    }
+
+    return (
+      <div className='boardContainer'>
+        {board}
+      </div>
+    );
+}
+
+export default Board;
---
+++ frontend/src/components/square.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './square.css'
+
+function Square(props) {
+  return (
+    <div className={`square ${props.color}`}>
+    </div>
+  );
+}
+
+export default Square;
