0a2f8e38889ef1a442490f04f4e2c67d91be7ab4
PuSung <sam90.hsu@gmail.com>
2022-12-18 16:40:11 +0800

    finish move

--- backend/src/play_game.js
+++ backend/src/play_game.js
@@ -2,20 +2,4 @@ import { POS } from "./constants.js"
 import { Game } from "./Game.js"
 
 let game = new Game()
-const readline = require( 'readline' ).createInterface( {
-    input: process.stdin,
-    output: process.stdout,
-} )
-
-readline.question( `What's your name?`, name => {
-    console.log( `Hi ${name}!` )
-    readline.close()
-} )
-while ( true ) {
-
-}
-
-
-game.preview( POS )
 game.draw_board()
-game.draw_board( 'ava' )
