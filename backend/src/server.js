09fa61432c79d3b6cb46d8c0509d7cae842fdf14
changliseng <changliseng0903@gmail.com>
2022-12-18 16:24:45 +0800

    server connectioin

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,15 @@ import http from 'http'
 import dotenv from "dotenv-defaults";
 import mongoose from "mongoose";
 import WebSocket from 'ws';
+import wsConnect from "./wsConnect"
 
 const app = express()
 const server = http.createServer(app)
 const serverWS = new WebSocket.Server({server})
+
+serverWS.on("connection", (ws) => {
+    ws.onmessage = wsConnect.do(ws, serverWS);
+})
 
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
