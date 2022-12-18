8c5498c84bedfd1201f707709f57cbe5e62f23e6
PuSung <sam90.hsu@gmail.com>
2022-12-19 02:42:22 +0800

    frontend init function

--- backend/src/wsConnect.js
+++ backend/src/wsConnect.js
@@ -1,40 +1,45 @@
 import { Game } from "./Game.js"
 
-const sendData = (data, ws) => {
-    ws.send(JSON.stringify(data));
-}   
+const sendData = ( data, ws ) => {
+    ws.send( JSON.stringify( data ) )
+}
 
-const boardcastMessage = (wss, data) => {
-    wss.clients.forEach(client => {
-        sendData(data, client);
-    });
+const boardcastMessage = ( wss, data ) => {
+    wss.clients.forEach( client => {
+        sendData( data, client )
+    } )
 }
 
 export default {
     game: '',
-    init:(ws) => {
-        game = new Game();
+    init: ( ws ) => {
+        game = new Game()
     },
 
-    do: (ws, wss, game) => {
-        return((async (byteString) => {
-            const { data } = byteString;
-            const [task, payload] = JSON.parse(data);
-            switch (task){
-                case "preview" : {
-                    const location = payload;
-                    const response = game.preview([location[0], location[1]]);
-                    // console.log(game)
-                    boardcastMessage(wss, response);
-                    break;
+    do: ( ws, wss, game ) => {
+        return ( ( async ( byteString ) => {
+            const { data } = byteString
+            const [task, payload] = JSON.parse( data )
+            switch ( task ) {
+                case "init": {
+                    console.log( 'init' )
+                    const response = game.board
+                    boardcastMessage( wss, response )
+                    break
                 }
-
-                case "move" :{
-                    const {from, to} = payload;
-                    const response = game.move(from, to);
-                    boardcastMessage(wss, response);
+                case "preview": {
+                    const location = payload
+                    const response = game.preview( [location[0], location[1]] )
+                    boardcastMessage( wss, response )
+                    break
+                }
+                case "move": {
+                    const { from, to } = payload
+                    const response = game.move( from, to )
+                    boardcastMessage( wss, response )
+                    break
                 }
             }
-        }))
+        } ) )
     }
 }
--- frontend/src/containers/hooks/useChess.js
+++ frontend/src/containers/hooks/useChess.js
@@ -23,21 +23,15 @@ const ChessProvider = ( props ) => {
     const [board, setBoard] = useState( [] )
     const [focusP, setFocusP] = useState( [] )
 
-
     clientWS.onmessage = ( byteString ) => {
         const { data } = byteString
         const newBoard = JSON.parse( data )
         setBoard( newBoard )
     }
 
-
     useEffect( () => {
-        setBoard( init() )
+        init()
     }, [] )
-
-    useEffect( () => {
-        console.log( board )
-    }, [board] )
 
     const preview = ( previewPos ) => {
         // get preview board 
@@ -47,47 +41,11 @@ const ChessProvider = ( props ) => {
 
     const move = ( from, to ) => {
         // get moved board 
-        sendData(["move", {from, to}])
+        sendData( ["move", { from, to }] )
     }
 
     const init = () => {
-        let test_board = []
-        for ( let i = 0; i < 8; i++ ) {
-            test_board.push( [] )
-            for ( let j = 0; j < 8; j++ ) {
-                test_board[i].push( {
-                    type: 'nothing',
-                    color: ( i == 0 || i == 1 ) ? 'b' : ( ( i == 6 || i == 7 ) ? 'w' : 'nothing' ),
-                    ava: false
-                }
-                )
-            }
-        }
-
-        test_board[0][0].type = 'rook'
-        test_board[0][1].type = 'knight'
-        test_board[0][2].type = 'bishop'
-        test_board[0][3].type = 'queen'
-        test_board[0][4].type = 'king'
-        test_board[0][5].type = 'bishop'
-        test_board[0][6].type = 'knight'
-        test_board[0][7].type = 'rook'
-        for ( let y = 0; y < 8; y++ ) {
-            test_board[1][y].type = 'pawn'
-        }
-
-        test_board[7][0].type = 'rook'
-        test_board[7][1].type = 'knight'
-        test_board[7][2].type = 'bishop'
-        test_board[7][3].type = 'queen'
-        test_board[7][4].type = 'king'
-        test_board[7][5].type = 'bishop'
-        test_board[7][6].type = 'knight'
-        test_board[7][7].type = 'rook'
-        for ( let y = 0; y < 8; y++ ) {
-            test_board[6][y].type = 'pawn'
-        }
-        return test_board
+        sendData( ['init'] )
     }
 
     return (
