9be4e3003ad714ad12bae7e22a8e001ec5eb6f7b
PuSung <sam90.hsu@gmail.com>
2022-12-19 15:06:47 +0800

    small changes

--- frontend/src/components/WaitModal.js
+++ frontend/src/components/WaitModal.js
@@ -2,18 +2,19 @@ import styled from "styled-components"
 
 const WaitModalWrapper = styled.div`
     height: 100px;
-    width: 400px;
+    width: 450px;
     position: absolute;
     background-color: white;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     opacity: 0.5;
+    border-radius: 20px;
 `
 
 const WaitTextWrapper = styled.div`
     height:100px;
-    width:350px;
+    width:400px;
     position: absolute;
     top: 50%;
     left: 50%;
@@ -31,7 +32,7 @@ const WaitModal = () => {
             <WaitModalWrapper>
             </WaitModalWrapper>
             <WaitTextWrapper>
-                <p>Waiting for opponent</p>
+                <p>♛ Waiting for opponent ♛</p>
             </WaitTextWrapper>
         </>
     )
